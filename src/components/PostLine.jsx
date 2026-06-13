be8a6e2e4f54dc7691e0645dc4765efac4f8f293
Jason LeGlue <jasonleglue@gmail.com>
2026-06-13 09:51:59 -0500

    Added some css to All Posts components. Made the topic area in PostLine.jsx display the name of topic instead of topic Id

--- src/components/PostLine.jsx
+++ src/components/PostLine.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { getAllPosts } from "../services/postService";
+import "./PostLines.css"
 
 export const PostLine = ({ post }) => {
     return (
         <>
           <div className="postDisplay">
             <div className="postTitle">{post.title}</div>
-            <div className="postTopic">{post.topicId}</div>
-            <div className="postLikes">&#65039;</div>  
+            <div className="postTopic">{post.topic.name}</div>
+            <div className="postLikes">&#10084;</div>  
           </div>  
         </>
     )
